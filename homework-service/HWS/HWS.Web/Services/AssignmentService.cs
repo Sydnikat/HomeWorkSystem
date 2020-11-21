5adc510a340812d069f48fb7776490735e3d819b
David Szalai <tonycalifor1@gmail.com>
2020-11-21 21:56:53 +0100

    Create service for assignments

---
+++ homework-service/HWS/HWS.Web/Services/AssignmentService.cs
@@ -0,0 +1,29 @@
+﻿using HWS.Dal.Sql.Assignments;
+using HWS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HWS.Services
+{
+    public class AssignmentService : IAssignmentService
+    {
+        private readonly IAssignmentRepository assignmentRepository;
+
+        public AssignmentService(IAssignmentRepository assignmentRepository)
+        {
+            this.assignmentRepository = assignmentRepository;
+        }
+
+        public async Task<ICollection<Assignment>> GetAssignmentsForStudent(User student)
+        {
+            return (await assignmentRepository.FindAllByStudent(student).ConfigureAwait(false)).ToList();
+        }
+
+        public async Task<ICollection<Assignment>> GetAssignmentsForTeacher(User teacher)
+        {
+            return (await assignmentRepository.FindAllByUserInGraders(teacher).ConfigureAwait(false)).ToList();
+        }
+    }
+}
---
+++ homework-service/HWS/HWS.Web/Services/IAssignmentService.cs
@@ -0,0 +1,15 @@
+﻿using HWS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HWS.Services
+{
+    public interface IAssignmentService
+    {
+        Task<ICollection<Assignment>> GetAssignmentsForStudent(User student);
+
+        Task<ICollection<Assignment>> GetAssignmentsForTeacher(User teacher);
+    }
+}
