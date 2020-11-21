3f2deaa2d93af8d2cc4c803e21605c0946b7c2bc
David Szalai <tonycalifor1@gmail.com>
2020-11-21 17:46:10 +0100

    Extend groupStore with new homework handling

--- web-client/src/store/groupStore.ts
+++ web-client/src/store/groupStore.ts
@@ -1,5 +1,6 @@
 import {IGroupResponse} from "../models/group";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {IHomeworkResponse} from "../models/homework";
 
 export interface GroupState {
   groups: IGroupResponse[];
@@ -25,12 +26,26 @@ export const group = createSlice({
       action: PayloadAction<IGroupResponse>
     ) {
       state.groups = [action.payload, ...state.groups];
+    },
+
+    updateGroupHomeworks(
+      state: GroupState,
+      action: PayloadAction<IHomeworkResponse>
+    ) {
+      const homework = action.payload;
+      const index = state.groups.findIndex(g => g.id === homework.groupId);
+
+      if (index !== -1) {
+        state.groups[index].homeworks.push(homework);
+      }
     }
+
   }
 });
 
 export const {
   setGroups,
-  addNewGroup
+  addNewGroup,
+  updateGroupHomeworks
 } = group.actions;
 export const groupReducer = group.reducer;
