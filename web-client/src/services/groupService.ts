11e588ef6245fb9f88b9f83a13399f344cdfcea0
David Szalai <tonycalifor1@gmail.com>
2020-11-22 20:47:43 +0100

    Remove test data

--- web-client/src/services/groupService.ts
+++ web-client/src/services/groupService.ts
@@ -27,10 +27,10 @@ export const groupService: GroupService = {
       if (response.status === 200) {
         return response.data as IGroupResponse[];
       }
-      return exampleGroups;
+      return [];
     } catch (err) {
       console.log(err);
-      return exampleGroups;
+      return [];
     }
   },
 
@@ -56,10 +56,10 @@ export const groupService: GroupService = {
       if (response.status === 200) {
         return response.data as ICommentResponse[];
       }
-      return exampleComments;
+      return [];
     } catch (err) {
       console.log(err);
-      return exampleComments;
+      return [];
     }
   },
 
@@ -114,96 +114,3 @@ export const groupService: GroupService = {
     }
   },
 };
-
-const exampleGroups: IGroupResponse[] = [
-  {
-    id: "g1",
-    name: "Csoport1",
-    code: "abc123",
-    ownerId: "8ab2c6a8-ab4a-42dc-97e2-668e273b5837",
-    ownerFullName: "Oktató1",
-    students: [],
-    teachers: [],
-    homeworks: [
-      {
-        id: "h1",
-        title: "Házi feladat beadó rendszer",
-        description: "Többfelhasználós rendszer",
-        maxFileSize: 50,
-        groupId: "g1",
-        submissionDeadline: "2020. 11. 12.",
-        applicationDeadline: "2020. 10. 15.",
-        maximumNumberOfStudents: 2,
-        currentNumberOfStudents: 2,
-        graders: ["Oktató1", "Oktató2"],
-        students: ["Hallgató1", "Hallgató2"],
-      },
-      {
-        id: "h3",
-        title: "Szorgalmi házi feladat",
-        description:
-          "Extra feladat plusz pontért. Ez egy hosszú leírása a házi feladatnak, több sorban is kifér.",
-        maxFileSize: 50,
-        groupId: "g1",
-        submissionDeadline: "2020. 11. 20.",
-        maximumNumberOfStudents: 2,
-        currentNumberOfStudents: 2,
-        graders: ["Oktató1", "Oktató2"],
-        students: ["Hallgató2"],
-      },
-      {
-        id: "h4",
-        title: "Második szorgalmi házi feladat",
-        description:
-          "Extra feladat plusz pontért. Ez egy hosszú leírása a házi feladatnak, több sorban is kifér.",
-        maxFileSize: 50,
-        groupId: "g1",
-        submissionDeadline: "2020. 11. 27.",
-        applicationDeadline: "2020. 10. 15.",
-        maximumNumberOfStudents: 2,
-        currentNumberOfStudents: 2,
-        graders: ["Oktató1", "Oktató2"],
-        students: ["Hallgató2"],
-      },
-    ],
-  },
-  {
-    id: "g2",
-    name: "Csoport2",
-    code: "abc456",
-    ownerId: "ownerid2",
-    ownerFullName: "Oktató2",
-    students: [],
-    teachers: [],
-    homeworks: [
-      {
-        id: "h2",
-        title: "Nagy házi",
-        description: "Nagy házi leírás",
-        maxFileSize: 50,
-        groupId: "g2",
-        submissionDeadline: "2020. 11. 12.",
-        applicationDeadline: "2020. 10. 15.",
-        maximumNumberOfStudents: 20,
-        currentNumberOfStudents: 2,
-        graders: ["Oktató1", "Oktató2"],
-        students: ["Hallgató1", "Hallgató2"],
-      },
-    ],
-  },
-];
-
-const exampleComments: ICommentResponse[] = [
-  {
-    id: "c1",
-    createdBy: "hallgato1",
-    creationDate: "2020. 11. 16.",
-    content: "Mikor lesz a ZH?",
-  },
-  {
-    id: "c2",
-    createdBy: "hallgato2",
-    creationDate: "2020. 11. 17.",
-    content: "Holnap.",
-  },
-];
